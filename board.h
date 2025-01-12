46b249f539db1afc4005f69635705e42c26fb33b
NNNNNiShimoto <nishimotos@NishimotonoMacBook-Air.local>
2025-01-12 22:27:19 +0900

    extract board.h

---
+++ board.h
@@ -0,0 +1,41 @@
+#include <memory>
+#include <vector>
+
+enum class Color {
+    NONE    = 0b000,
+    RED     = 0b100,
+    GREEN   = 0b010,
+    BLUE    = 0b001,
+    YELLOW  = 0b110,
+    MAGENTA = 0b101,
+    CYAN    = 0b011,
+    WHITE   = 0b111
+};
+
+inline Color operator|(Color a, Color b) {
+    using T = std::underlying_type_t<Color>;
+    return static_cast<Color>(static_cast<T>(a) | static_cast<T>(b));
+}
+
+struct Cursor {
+    int x;
+    int y;
+};
+
+struct Cell {
+    Color mineColor; //R,G,B or none only
+    bool isOpened;
+    bool isFlag;
+    Color flagColor; //R,G,B or none only
+    int mineNumber; 
+    Color mineNumberColor;
+};
+
+struct Board{
+    std::unique_ptr<Cell[]> cells;
+    std::unique_ptr<std::vector<int>> mineIdxList;
+    int redMineNum;
+    int greenMineNum;
+    int blueMineNum;
+    int remainCellNum;
+};
--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <sstream>
 
 #include "colortext.h"
+#include "board.h"
 
 using namespace std;
 
@@ -15,45 +16,6 @@ using namespace std;
 #define MINE_NUM 5
 
 termios original;
-
-enum class Color {
-    NONE    = 0b000,
-    RED     = 0b100,
-    GREEN   = 0b010,
-    BLUE    = 0b001,
-    YELLOW  = 0b110,
-    MAGENTA = 0b101,
-    CYAN    = 0b011,
-    WHITE   = 0b111
-};
-
-inline Color operator|(Color a, Color b) {
-    using T = std::underlying_type_t<Color>;
-    return static_cast<Color>(static_cast<T>(a) | static_cast<T>(b));
-}
-
-struct Cursor {
-    int x;
-    int y;
-};
-
-struct Cell {
-    Color mineColor; //R,G,B or none only
-    bool isOpened;
-    bool isFlag;
-    Color flagColor; //R,G,B or none only
-    int mineNumber; 
-    Color mineNumberColor;
-};
-
-struct Board{
-    unique_ptr<Cell[]> cells;
-    unique_ptr<vector<int>> mineIdxList;
-    int redMineNum;
-    int greenMineNum;
-    int blueMineNum;
-    int remainCellNum;
-};
 
 //recover terminal 
 void disableRawMode() {
